2638427ca959f27ef762be53fe73c46ecbefa580
Shreya <shreyapandey4822@gmail.com>
2026-06-11 23:17:43 +0530

    changes done

--- DailyAssignments/Day3/src/main/java/org/com/masstechBuisnessSolutions/EmpBonusCalculator.java
+++ DailyAssignments/Day3/src/main/java/org/com/masstechBuisnessSolutions/EmpBonusCalculator.java
@@ -6,44 +6,56 @@ public class EmpBonusCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter employee's name");
         String emp_name = sc.nextLine();
+
         System.out.println("Enter salary");
         double salary = sc.nextDouble();
-        System.out.println("Enter performance rating");
+
+        System.out.println("Enter performance rating for e.g A,B,C,D");
         String performanceRating = sc.next();
 
         switch (performanceRating) {
+
             case "A":
-                double bonusAmount= 0.2*salary;
+                double bonusAmount = 0.2 * salary;
                 System.out.println("Employee name is: " + emp_name);
-                System.out.println("Employee salary is: "+ salary);
-                System.out.println("Rating is : "+ performanceRating);
-                System.out.println("Bonus Amount: "+ bonusAmount);
-                System.out.println("Final salary: "+ bonusAmount + salary);
+                System.out.println("Employee salary is: " + salary);
+                System.out.println("Rating is: " + performanceRating);
+                System.out.println("Bonus Amount: " + bonusAmount);
+                System.out.println("Final salary: " + (bonusAmount + salary));
+                break;
+
             case "B":
-                double bonusAmountOfB= 0.15 * salary;
+                double bonusAmountOfB = 0.15 * salary;
                 System.out.println("Employee name is: " + emp_name);
-                System.out.println("Employee salary is: "+ salary);
-                System.out.println("Rating is : "+ performanceRating);
-                System.out.println("Bonus Amount: "+ bonusAmountOfB);
-                System.out.println("Final salary: "+ bonusAmountOfB + salary);
+                System.out.println("Employee salary is: " + salary);
+                System.out.println("Rating is: " + performanceRating);
+                System.out.println("Bonus Amount: " + bonusAmountOfB);
+                System.out.println("Final salary: " + (bonusAmountOfB + salary));
+                break;
+
             case "C":
-                double bonusAmountOfC= 0.1*salary;
+                double bonusAmountOfC = 0.1 * salary;
                 System.out.println("Employee name is: " + emp_name);
-                System.out.println("Employee salary is: "+ salary);
-                System.out.println("Rating is : "+ performanceRating);
-                System.out.println("Bonus Amount: "+ bonusAmountOfC);
-                System.out.println("Final salary: "+ bonusAmountOfC + salary);
+                System.out.println("Employee salary is: " + salary);
+                System.out.println("Rating is: " + performanceRating);
+                System.out.println("Bonus Amount: " + bonusAmountOfC);
+                System.out.println("Final salary: " + (bonusAmountOfC + salary));
+                break;
+
             case "D":
-                double bonusAmountOfD= 0.05 *salary;
+                double bonusAmountOfD = 0.05 * salary;
                 System.out.println("Employee name is: " + emp_name);
-                System.out.println("Employee salary is: "+ salary);
-                System.out.println("Rating is : "+ performanceRating);
-                System.out.println("Bonus Amount: "+ bonusAmountOfD);
-                System.out.println("Final salary: "+ bonusAmountOfD + salary);
+                System.out.println("Employee salary is: " + salary);
+                System.out.println("Rating is: " + performanceRating);
+                System.out.println("Bonus Amount: " + bonusAmountOfD);
+                System.out.println("Final salary: " + (bonusAmountOfD + salary));
+                break;
 
-
+            default:
+                System.out.println("Invalid Performance Rating");
         }
 
 
