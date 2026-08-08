d344598ffa220f72bc270c7f1db8d52d1c39dc00
Shreya Pandey <shreyapandey4822@gmail.com>
2026-08-08 12:32:03 +0530

    hrms saturday schema

--- orderBill/src/main/java/com/example/demo/controller/OrderController.java
+++ orderBill/src/main/java/com/example/demo/controller/OrderController.java
@@ -1,14 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.entities.Orders;
-import com.example.demo.service.OrderService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import com.example.demo.entities.Orders;
 @Controller
 public class OrderController {
     @Autowired
